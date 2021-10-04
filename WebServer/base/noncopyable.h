d12406a688fde514cadbbff6034c3d2742be06dc
ruanyuan <2561977833@qq.com>
2021-10-04 21:35:32 +0800

    init

---
+++ WebServer/base/noncopyable.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class noncopyable{
+    protected:
+    noncopyable() = default;
+    ~noncopyable() = default;
+
+    private:
+    noncopyable(const noncopyable&);
+    const noncopyable& operator=(const noncopyable&);
+
+};
