d3df8f0d4e82076ed502546fc2684b44f3ea1146
ruanyuan <2561977833@qq.com>
2021-10-05 13:04:56 +0800

    init

---
+++ WebServer/base/Thread.cpp
@@ -0,0 +1,92 @@
+#include <assert.h>
+#include <errno.h>
+#include <linux/unistd.h>
+#include <pthread.h>
+#include <sched.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string>
+#include <sys/prctl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <memory>
+#include "Thread.h"
+#include "CurrentThread.h"
+#include <iostream>
+using namespace std;
+
+namespace CurrentThread {
+    __thread int t_cachedTid=0;
+    __thread char t_tidString[32];
+    __thread int t_tidStringLength =6;
+    __thread const char* t_threadName = "default";
+}
+
+struct ThreadData{
+    typedef Thread::ThreadFunc ThreadFunc;
+    ThreadFunc func_;
+    string name_;
+    pid_t* tid_;
+    CountDownLatch* latch_;
+
+    ThreadData(const ThreadFunc& func, const string& name, pid_t* tid, CountDownLatch* latch)
+            : func_(func), name_(name), tid_(tid), latch_(latch) { }
+
+    void runInThread(){
+        *tid_=CurrentThread::tid();
+        tid_=nullptr;
+        latch_->countDown();
+        latch_=nullptr;
+
+        CurrentThread::t_threadName = name_.empty()?"Thread":name_.c_str();
+        prctl(PR_SET_NAME, CurrentThread::t_threadName);
+
+        func_();
+        CurrentThread::t_threadName = "finished";
+    }
+};
+
+void* startThread(void* obj){
+    ThreadData* data = static_cast<ThreadData*>(obj);
+    data->runInThread();
+    delete data;
+    return nullptr;
+}
+
+Thread::Thread(const ThreadFunc& func, const string& n)
+        :started_(false), joined_(false), pthreadId_(0),
+        tid_(0), func_(func), name_(n), latch_(1){
+            setDefaultName();
+}
+
+Thread::~Thread(){
+    if(started_&&!joined_) pthread_detach(pthreadId_);
+}
+
+void Thread::setDefaultName(){
+    if(name_.empty()){
+        char buf[32];
+        snprintf(buf, sizeof(buf), "Thread");
+        name_=buf;
+    }
+}
+
+void Thread::start(){
+    assert(!started_);
+    started_=true;
+    ThreadData* data=new ThreadData(func_,name_,&tid_,&latch_);
+    if(pthread_create(&pthreadId_, nullptr, &startThread, data)){
+        started_=false;
+        delete data;
+    }else {
+        latch_.wait();
+        assert(tid_>0);
+    }
+}
+
+int Thread::join(){
+    assert(started_);
+    assert(!joined_);
+    joined_=true;
+    return pthread_join(pthreadId_, nullptr);
+}
