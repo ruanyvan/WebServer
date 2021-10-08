93f62a2827f2240a8aff23d25ce2e40f5a9aed14
ruanyuan <2561977833@qq.com>
2021-10-08 17:19:06 +0800

    init

--- WebServer/Server.cpp
+++ WebServer/Server.cpp
@@ -1,12 +1,15 @@
-#include"Server.h"
-#include<arpa/inet.h>
+#include "Server.h"
+#include <arpa/inet.h>
 #include <cstring>
-#include<netinet/in.h>
-#include<sys/socket.h>
-#include<functional>
+#include <memory>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <functional>
 #include <unistd.h>
-#include"Util.h"
-#include"base/Logging.h"
+#include "EventLoop.h"
+#include "Util.h"
+#include "base/Logging.h"
 
 Server::Server(EventLoop* loop, int threadNum, int port)
         : loop_(loop),
@@ -38,7 +41,38 @@ void Server::handNewConn(){
     memset(&client_addr, 0, sizeof(struct sockaddr_in));
     socklen_t client_addr_len = sizeof(client_addr);
     int accept_fd = 0;
-    
-    
-}
 
+    while((accept_fd = accept(listenFd_, (struct sockaddr *)&client_addr, 
+            &client_addr_len))<0){
+        EventLoop* loop = eventLoopThreadPool_->getNextLoop();
+        LOG<<"New connection from "<<inet_ntoa(client_addr.sin_addr)<<":"<<ntohs(client_addr.sin_port);
+        // cout << "new connection" << endl;
+        // cout << inet_ntoa(client_addr.sin_addr) << endl;
+        // cout << ntohs(client_addr.sin_port) << endl;
+        /*
+        // TCP的保活机制默认是关闭的
+        int optval = 0;
+        socklen_t len_optval = 4;
+        getsockopt(accept_fd, SOL_SOCKET,  SO_KEEPALIVE, &optval, &len_optval);
+        cout << "optval ==" << optval << endl;
+        */
+        // 限制服务器的最大并发连接数
+        if(accept_fd >= MAXFDS){
+            close(accept_fd);
+            continue;
+        }
+
+        // 设为非阻塞模式
+        if(setSocketNonBlocking(accept_fd) < 0){
+            LOG<<"Set non block failed!";
+            return;
+        }
+
+        setSocketNodelay(accept_fd);
+
+        shared_ptr<HttpData> req_info(new HttpData(loop, accept_fd));
+        req_info->getChannel()->setHolder(req_info);
+        loop_->queueInLoop(std::bind(&HttpData::newEvent, req_info));
+    }
+    acceptChannel_->setEvents(EPOLLIN|EPOLLET);
+}
